e799a4446047f0e0ad28ca31fa1284897abb4752
Koushik Kar <koushikkar712@gmail.com>
2026-04-26 19:33:21 +0530

    Created the complete folder structure ig

---
+++ src/app/documentations/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DocDirector = () => {
+  return (
+    <div>DocDirector
+    
+    </div>
+  )
+}
+
+export default DocDirector
---
+++ src/app/documentations/postgre/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostGreSQL = () => {
+  return (
+    <div>PostGreSQL</div>
+  )
+}
+
+export default PostGreSQL
